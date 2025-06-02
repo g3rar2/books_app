8ad55b866dfb4a729713f89adc1c7c0098757e2a
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-06-01 19:37:30 -0600

    Post

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ err? console.log("No se pudo conectar a la base de datos"):console.log("Conexion
 })
 
 
+app.use(express.json());
+
 app.get('/api/libros',(req,res)=>{
-    const query = "select * from libros";
-    pool.query(query,(err,result)=>{
+    const sql = "select * from libros";
+    pool.query(sql,(err,result)=>{
         if(err){
             return res.status(500).json({status:500,message:"Error en la consulta",data:null});
         }else{
@@ -29,18 +31,36 @@ app.get('/api/libros',(req,res)=>{
 
 app.get('/api/libros/:codigo',(req,res)=>{
     const codigo=parseInt(req.params.codigo)
-    const query = "select * from libros where codigo = ?";
-    pool.query(query,codigo,(err,result)=>{
+    const sql = "select * from libros where codigo = ?";
+    pool.query(sql,codigo,(err,result)=>{
         if(err){
             return res.status(500).json({status:500,message:"Error en la consulta",data:null});
         }else{
-            res.status(200).json({status:200,message:"Sucess",data:result});
+          return  res.status(200).json({status:200,message:"Sucess",data:result});
         }
     })
 })
 
 
-app.use(express.json());
+app.post('/api/libros',(req,res)=>{
+    const libro=req.body;
+    const sql = "insert into libros (titulo,autor,anio) values (?,?,?)";
+    pool.query(sql,[libro.titulo,libro.autor,libro.anio],(err,result)=>{
+        if(err){
+            return res.status(500).json({status:500,message:"Error",data:null});
+        }else{
+            libro.codigo=result.insertId;
+           return  res.status(200).json({status:200,message:"Sucess",data:libro});
+        }
+    })
+})
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
