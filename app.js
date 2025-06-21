ae729d8dd7fba99bbf9c62ed5a52f9cfa29ff96c
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-06-20 19:12:57 -0600

    Axios

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const axios = require('axios');
+const cors = require('cors');
 const pool=require('./config/db.js');
 const authMiddleware=require('./middleware/authMiddleware.js');
 const app = express();
@@ -11,6 +13,7 @@ const libros = require('./routes/libros.js');
 
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/',authLogin)
 app.use('/',libros)
@@ -24,6 +27,36 @@ app.get('/api/gethash/:plainText',authMiddleware ,async (req,res)=>{
     return res.send(hash);
 })
 
+
+
+
+//---------------------------- API DE TERCEROS---------------------------------------
+
+
+
+app.get('/api/posts', async (req,res)=>{
+try{
+    let url=process.env.ENDPOINT_API_TERCEROS+"/posts";
+    const axiosResponse= await axios.get(url)
+    res.status(200).json({status:200, message:"Success",data: axiosResponse.data})
+}catch(e){
+    res.status(500).json({status:500,message:"Internal Server Error"})
+}
+})
+
+app.post('/api/posts', async (req,res)=>{
+    const post=req.body;
+    try{
+        let url=process.env.ENDPOINT_API_TERCEROS+"/posts";
+        const axiosResponse= await axios.post(url,post)
+        res.status(200).json({status:200, message:"Success",data: axiosResponse.data})
+    }catch(e){
+        res.status(500).json({status:500,message:"Internal Server Error"})
+    }
+})
+
+
+//---------------------------- API DE TERCEROS---------------------------------------
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
